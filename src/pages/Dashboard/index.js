efbf79902a57ef8c696c5f0bd33967ef462bb3d0
therealeddy <eduardo.mfonseca@hotmail.com>
2020-05-30 18:40:07 -0300

    feat: dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
+import pt from 'date-fns/locale/pt';
 
-function Dashboard() {
-  return <div />;
+import api from '~/services/api';
+
+import { Container, Time } from './styles';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+  const [schedule, setSchedule] = useState([]);
+
+  const dateFormatted = useMemo(
+    () =>
+      format(date, "d 'de' MMMM", {
+        locale: pt,
+      }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map((hour) => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appoitment: response.data.find((a) =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+
+      setSchedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft color="#FFF" size={36} />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight color="#FFF" size={36} />
+        </button>
+      </header>
+      <ul>
+        {schedule.map((time) => (
+          <Time past={time.past} key={time.time} available={!time.appoitment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appoitment ? time.appoitment.user.name : 'Em aberto!'}
+            </span>
+          </Time>
+        ))}
+      </ul>
+    </Container>
+  );
 }
-
-export default Dashboard;
